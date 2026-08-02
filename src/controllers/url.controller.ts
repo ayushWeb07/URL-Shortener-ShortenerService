f72d28637d47b668ce96ccee7fe39cfdd73b70ec
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-03 01:27:14 +0530

    feat: Create the urls router in tRPC

---
+++ src/controllers/url.controller.ts
@@ -0,0 +1,122 @@
+import { publicProcedure } from "../trpc/context.ts";
+import { z } from "zod";
+import * as urlService from "../services/url.service.ts";
+import { TRPCError } from "@trpc/server";
+
+const urlController = {
+	createUrl: publicProcedure
+		.input(
+			z.object({
+				originalUrl: z.url(),
+			}),
+		)
+		.mutation(async (opts) => {
+			try {
+				const { input } = opts;
+
+				// call the url service
+				const newUrl = await urlService.createUrl(input.originalUrl);
+
+				return {
+					message: "Successfully shortened the url",
+					...newUrl,
+				};
+			} catch (error) {
+				throw new TRPCError({
+					code: "INTERNAL_SERVER_ERROR",
+					message:
+						"An unexpected error occurred while shortening the url, please try again later.",
+					cause: error,
+				});
+			}
+		}),
+
+	findAllUrls: publicProcedure.query(async () => {
+		try {
+			// call the url service
+			const urls = await urlService.findAllUrls();
+
+			return {
+				message: "Successfully fetched all the urls",
+				...urls,
+			};
+		} catch (error) {
+			throw new TRPCError({
+				code: "INTERNAL_SERVER_ERROR",
+				message:
+					"An unexpected error occurred while fetching all the urls, please try again later.",
+				cause: error,
+			});
+		}
+	}),
+
+	findUrlById: publicProcedure
+		.input(z.coerce.number().int().nonnegative())
+		.query(async (opts) => {
+			try {
+				const { input } = opts;
+
+				// call the url service
+				const url = await urlService.findUrlById(input);
+
+				return {
+					message: "Successfully fetched the url by id",
+					...url,
+				};
+			} catch (error) {
+				throw new TRPCError({
+					code: "INTERNAL_SERVER_ERROR",
+					message:
+						"An unexpected error occurred while fetching the url by id, please try again later.",
+					cause: error,
+				});
+			}
+		}),
+
+	findUrlByShortUrl: publicProcedure
+		.input(z.coerce.string().min(1).max(10).lowercase())
+		.query(async (opts) => {
+			try {
+				const { input } = opts;
+
+				// call the url service
+				const url = await urlService.findUrlByShortUrl(input);
+
+				return {
+					message: "Successfully fetched the url by short url",
+					url,
+				};
+			} catch (error) {
+				throw new TRPCError({
+					code: "INTERNAL_SERVER_ERROR",
+					message:
+						"An unexpected error occurred while fetching the url by short url, please try again later.",
+					cause: error,
+				});
+			}
+		}),
+
+	deleteUrlById: publicProcedure
+		.input(z.coerce.number().int().nonnegative())
+		.mutation(async (opts) => {
+			try {
+				const { input } = opts;
+
+				// call the url service
+				await urlService.deleteUrlById(input);
+
+				return {
+					message: "Successfully deleted the url by id",
+				};
+			} catch (error) {
+				throw new TRPCError({
+					code: "INTERNAL_SERVER_ERROR",
+					message:
+						"An unexpected error occurred while deleting the url by id, please try again later.",
+					cause: error,
+				});
+			}
+		}),
+};
+
+export { urlController };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,10 +2,12 @@ import { router } from "../context.ts";
 
 import { userRouter } from "./user.router.ts";
 import { postRouter } from "./post.router.ts";
+import { urlRouter } from "./url.router.ts";
 
 const appRouter = router({
 	user: userRouter,
 	post: postRouter,
+	url: urlRouter,
 });
 
 export { appRouter };
