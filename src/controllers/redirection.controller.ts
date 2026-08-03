4136b7c5efeed3db5fb080651a60abb3706cf830
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-03 10:12:45 +0530

    fix: Render trpc error in redirection path

--- src/controllers/redirection.controller.ts
+++ src/controllers/redirection.controller.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from "express";
 import { createCaller } from "../trpc/routers/_app.ts";
+import { TRPCError } from "@trpc/server";
+import { getHTTPStatusCodeFromError } from "@trpc/server/http";
 
 const redirectShortUrl = async (req: Request, res: Response) => {
 	try {
@@ -12,6 +14,15 @@ const redirectShortUrl = async (req: Request, res: Response) => {
 
 		res.redirect(urlData.url);
 	} catch (error) {
+		if (error instanceof TRPCError) {
+			const httpCode = getHTTPStatusCodeFromError(error);
+
+			res.status(httpCode).json({
+				name: error.name,
+				message: error.message,
+				cause: error?.cause || null,
+			});
+		}
 		throw error;
 	}
 };
